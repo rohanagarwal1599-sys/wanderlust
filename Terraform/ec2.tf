aa613549bfd6487946f27f6204f04e943b12c237
rohanagarwal1599-sys <rohanagarwal1599@gmail.com>
2025-12-28 20:48:06 +0530

    First Commit

---
+++ Terraform/ec2.tf
@@ -0,0 +1,82 @@
+resource "aws_key_pair" "deployer" {
+    key_name = "terra-key"
+    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFP26Gejg5zSV8N2kvn9flpSx+/6KUSvNwy9Sc+L5Vl4 lenovo@DESKTOP-SKI6JIM"
+}
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_security_group" "allow_user_to_connect" {
+    name = "allow TLS"
+    description = "Allow user to connect"
+    vpc_id = aws_default_vpc.default.id
+
+    ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description = "port 80 allow"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description = "port 443 allow"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description = "port 8080 allow"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description = "port 9000 allow"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+
+}
+
+resource "aws_instance" "Wanderlust" {
+    key_name = aws_key_pair.deployer.key_name
+    ami = var.ami_id
+    instance_type = var.instance_type
+    security_groups = [aws_security_group.allow_user_to_connect.name]
+    tags =  {
+        Name = "Automate"
+    }
+    root_block_device {
+      volume_size = 30
+      volume_type = "gp3"
+    }
+  
+}
